a451fddcce4109e02f434504b287fa3109b519aa
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-09-11 17:10:37 +0200

    Add to Category routes route for delete and update of single Category.

--- app/routes/categories.server.routes.js
+++ app/routes/categories.server.routes.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
 
   var categories = require('../../app/controllers/categories.server.controller');
 
-  // routes for Category model
+  // routes for Categories
   app.route('/categories')
 
     // route for GET categories
@@ -13,7 +13,14 @@ module.exports = function(app) {
     // route for POST categories
     .post(categories.create);
 
-    // route for GET single category
+  // routes for single Category
     app.route('/categories/:categoryId')
-      .get(categories.read);
+      // route for GET single category
+      .get(categories.read)
+
+      // route for UPDATE single category
+      .put(categories.update)
+
+      // route for DELETE single category
+      .delete(categories.delete)
 };
