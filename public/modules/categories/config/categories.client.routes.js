025784f67d2210472338b0110fb3738c7f7ccce2
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-09-21 16:44:59 +0200

    Add view for creating Category, modify angular routes.

--- public/modules/categories/config/categories.client.routes.js
+++ public/modules/categories/config/categories.client.routes.js
@@ -5,6 +5,10 @@ angular.module('categories').config(['$stateProvider',
 	function($stateProvider) {
 		// Categories state routing
 		$stateProvider.
+		state('create-category', {
+			url: '/categories/create',
+			templateUrl: 'modules/categories/views/create-category.client.view.html'
+		}).
 		state('categories', {
 			url: '/categories',
 			templateUrl: 'modules/categories/views/categories.client.view.html'
