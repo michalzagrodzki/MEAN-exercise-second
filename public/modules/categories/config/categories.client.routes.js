0bdadb2d5323c00bc35fae1a5461bdf44ca4ad0b
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-09-22 16:04:04 +0200

    Create routes for Categories edit, delete, update.

--- public/modules/categories/config/categories.client.routes.js
+++ public/modules/categories/config/categories.client.routes.js
@@ -5,13 +5,26 @@ angular.module('categories').config(['$stateProvider',
 	function($stateProvider) {
 		// Categories state routing
 		$stateProvider.
-		state('create-category', {
+      // CREATE
+		state('createCategory', {
 			url: '/categories/create',
 			templateUrl: 'modules/categories/views/create-category.client.view.html'
 		}).
-		state('categories', {
+      // READ Categories
+		state('listCategories', {
 			url: '/categories',
 			templateUrl: 'modules/categories/views/categories.client.view.html'
-		});
+		}).
+      // READ Single Category
+      state('viewCategory', {
+        url: '/categories/:categoryId',
+        templateUrl: 'modules/categories/views/view-category.client.view.html'
+    }).
+      // EDIT Single Category
+      state('editCategory', {
+        url: '/categories/:categoryId/edit',
+        templateUrl: 'modules/categories/views/edit-category.client.view.html'
+    });
+
 	}
 ]);
