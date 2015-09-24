672e0241793a6dd041d1c02997b9c39c2fb268e6
Michal Zagrodzki <michalzagrodzki@gmail.com>
2015-09-24 19:16:28 +0200

    Correct intendation.

--- public/modules/products/services/products.client.service.js
+++ public/modules/products/services/products.client.service.js
@@ -3,7 +3,8 @@
 //Categories service used to communicate Categories REST endpoints
 angular.module('products').factory('Products', ['$resource',
   function($resource) {
-    return $resource('products/:productId', { productId: '@_id'
+    return $resource('products/:productId', {
+      productId: '@_id'
     }, {
       update: {
         method: 'PUT'
